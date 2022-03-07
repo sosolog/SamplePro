45c99dfa5271be848d7a98db862fd970abb739a1
briso05 <sobright05@naver.com>
2022-03-07 12:15:17 +0900

    hotfix Branch 문자열 출력

--- src/GitTest.java
+++ src/GitTest.java
@@ -4,6 +4,8 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("GitTestMain");
 		System.out.println("Master Branch");
+
+		System.out.println("hotfix Branch");
 	}
 
 }
